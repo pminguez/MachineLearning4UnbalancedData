eb7ffb8f2577a46e2721c5d8e5aec25651c3dc86
pminguez <pablominguez@gmail.com>
2018-06-15 16:49:52 +0200

    Delete transpose_matrix.R

--- transpose_matrix.R
+++
@@ -1,9 +0,0 @@
-rm(list=ls())
-
-data.dir <- "/home/pablo/genetica/NeuralNetNeumo/data/"
-nas.file <- "repucri16052018_filtro.NAs.txt"
-nas.t.file <- "repucri16052018_filtro.NAs.t.txt"
-setwd(data.dir)
-
-nas.matrix <- read.table(nas.file, sep="\t", header=T)
-write.table(t(nas.matrix), file=nas.t.file,sep="\t", quote=F)
