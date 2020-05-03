e39364fe7688fb12a57e6f5d02687c157e7446bc
Pablo Minguez <pablominguez@gmail.com>
2020-05-03 07:31:43 +0200

    NN R script used to calculate and plot COVID-19 data

---
+++ nn4covind19.R
@@ -0,0 +1,330 @@
+rm(list=ls())
+
+library(DMwR)
+library(GGally)
+library(ggplot2)
+library(pROC)
+library(caret)
+library(pca3d)
+library(RColorBrewer)
+library(pheatmap)
+library(factoextra)
+library(reshape2)``
+library(tidyverse)
+library(pls)
+library(ggplot2)
+
+## PARAMETERS TO CUSTOMIZE
+#resampling <- "no" ## Say yes if the data is unbalanced
+
+k <- 10 ## k-cross fold validation
+times.the.size <- 1 #number of times that should multiply the cases to make the train/test datasets
+subsampling.times <- 10
+seed.sum <- 1
+lucky.number <- 101
+percentage.test <- 0.8
+black.and.white <- "no"
+
+#tags <- c("all","0-16","17-30","31-50","51-70","71-100","31-100")
+#tags <- c("all","31-50","51-70","71-100","31-100")
+
+## Takes values from command line
+args <- commandArgs(trailingOnly = TRUE)
+variable.type <- args[1] ## drugs or comobidities or clinical1
+tag <- args[2] ## age_range: "all","31-50","51-70","71-100","31-100"
+extra.tag <- args[3] ## UEvsREST (meaning: UCI and EXITUS vs REST); EXITUSvsREST; UCIvsEXITUS
+resampling <- args[4]
+data.dir <- arg [5]
+results.dir <- [6]
+
+
+input.file <- paste(data.dir, "COVID_4H_", variable.type,".", extra.tag, ".", tag, ".txt", sep="")
+matrix.frame <- read.table(input.file, sep="\t", header=T, na.strings = "NULL", 
+                           stringsAsFactors = F, colClasses = "numeric")
+
+## Subsampling j times
+number.to.keep <- sum(matrix.frame$OUTPUT==1)*times.the.size
+if(resampling=="no"){
+  number.to.keep <- sum(matrix.frame$OUTPUT==0)
+  subsampling.times <- 1
+}
+
+## Vectors that save each performance to calculate mean afterwards
+auc.vect <- rep(NA,k*subsampling.times) ## store the error in this vector (AUC)
+accuracy.vect <- rep(NA, k*subsampling.times) ## store the accuracy in this vector
+sensitivity.vector <- rep(NA, k*subsampling.times)
+specificity.vector <- rep(NA, k*subsampling.times)
+
+## Importance
+importance.frame <- data.frame(Parameter=character(0),value=numeric(0))
+
+count <- 0
+
+## Draw the first part of the roc curve
+if(black.and.white == "yes"){
+  my.colors <- rep("grey", 10)
+}else{
+  my.colors <- c("red", "deepskyblue", "yellow", "green4", "hotpink", "orange", "blueviolet", "navy", "sienna", "darksalmon")
+}
+
+## an object to save the AUC plot
+png(filename=paste(results.dir, "auc.", variable.type, ".", extra.tag, ".", tag, ".results.png", sep=""), width=500, height=480)
+
+# We repeat the analysis if we go for subsampling
+for (j in 1:subsampling.times){
+  matrix.frame <- read.table(input.file, sep="\t", header=T)
+  
+  first.value <- sum(matrix.frame$OUTPUT==1)+1
+  last.value <- dim(matrix.frame)[1]
+  rows <- c(1:sum(matrix.frame$OUTPUT==1), sample(first.value:last.value, number.to.keep, replace=F))
+  matrix.frame <- matrix.frame[rows,]
+  
+  ## We record vectors to draw a ROC curve per each resampling
+  classes.local.vector <- vector(mode="logical")
+  predictions.local.vector <- vector(mode="logical")
+  
+  ## 10-fold cross validation loop
+  for(i in 1:k){
+    set.seed(i + seed.sum + (j*100))
+    count <- count + 1
+    
+    index <- sample(1:nrow(matrix.frame),round(percentage.test*nrow(matrix.frame)))
+    
+    ## creating training and test sets
+    train.nn = matrix.frame[index, ]
+    test.nn = matrix.frame[-index, ]
+    
+    ## To avoid 
+    while(length(unique(test.nn$OUTPUT)) == 1 || length(unique(train.nn$OUTPUT)) == 1){
+      set.seed(i + seed.sum + (j*100) + lucky.number)
+      index <- sample(1:nrow(matrix.frame),round(0.9*nrow(matrix.frame)))
+      
+      ## creating training and test sets
+      train.nn = matrix.frame[index, ]
+      test.nn = matrix.frame[-index, ]
+      
+      lucky.number <- lucky.number + 1
+    }
+    
+    ## Convert response variable to factor (MORTALITY) to avoid NN does regression
+    train.nn$OUTPUT <- as.character(train.nn$OUTPUT)
+    train.nn$OUTPUT <- as.factor(train.nn$OUTPUT)
+    test.nn$OUTPUT <- as.character(test.nn$OUTPUT)
+    test.nn$OUTPUT <- as.factor(test.nn$OUTPUT)
+    
+    ## Get the model and prediciton
+    crtl <- trainControl(method="none",classProbs = T, summaryFunction=twoClassSummary)
+    my.grid <- expand.grid(.decay = seq(from = 0.1, to = 0.5, by = 0.1), .size = c(1,2,3,4,5))
+    nn.caret <- train(OUTPUT~., data=train.nn, method="nnet", trainControl=crtl, tuneGrid=my.grid)
+    
+    #plotnet(nn.caret)
+    predict_test.nn.prob <- predict(nn.caret, test.nn, type = "prob")
+    predict_test.nn.class <- predict(nn.caret, test.nn)
+    
+    t <- table(predictions=predict_test.nn.class, real=test.nn$OUTPUT) ## predictions vs real
+    t
+    
+    results <- data.frame(actual = test.nn$OUTPUT, prediction = predict_test.nn.prob$"0")
+    
+    ## Accuracy
+    accuracy.vect[count] <- sum(diag(t))/sum(t) ## percentage accuracy
+    
+    ## ROC element
+    my.roc <- roc(predictor=predict_test.nn.prob$"0", response=results$actual)
+    ## AUC calculated using all classes, other methods only allow two classes
+    auc.vect[count] <- my.roc$auc[1]
+    
+    ## Calculate sensitivity & specificity
+    specificity.vector[count] <- mean(mean(my.roc$specificities))
+    sensitivity.vector[count] <- mean(mean(my.roc$sensitivities))
+    
+    classes.local.vector <- c(classes.local.vector, as.numeric(levels(results$actual))[results$actual])
+    predictions.local.vector <- c(predictions.local.vector, results$prediction)
+    
+    ## Importance .. creo que estará bien ...
+    for(h in 1:length(colnames(matrix.frame)[1:length(colnames(matrix.frame))-1])){
+      measurement <- colnames(matrix.frame)[h]
+      importance.frame <- rbind(importance.frame, data.frame(Parameter=measurement, 
+                                                             value=as.vector(varImp(nn.caret)$importance[h,1])))
+    }
+  }
+  
+  ## Generate the ROC for a re-sampled matrix
+  intermediate.roc <- roc(factor(classes.local.vector), predictions.local.vector, ci=T)
+  
+  if(j==1){
+    plot.roc(intermediate.roc, col=my.colors[j], add=F)
+  }else{plot.roc(intermediate.roc, col=my.colors[j], add=T)}
+}
+
+dev.off()
+
+## Now deal with other paramenters (as before adding the ROC curve)
+#print(paste("Average AUC:", mean(auc.vect)))
+#print(paste("Average Accuracy:", mean(accuracy.vect)))
+#print(paste("Average Sensitivity:", mean(sensitivity.vector)))
+#print(paste("Average Specificity:", mean(specificity.vector)))
+
+## Print results to file
+results.frame <- data.frame(TYPE=character(0), AGE_RANGE=character(0), CONDITIONS=character(0),
+                            AUC=numeric(0),ACCURACY=numeric(0), 
+                            SENSITIVITY=numeric(0), 
+                            SPECIFICITY=numeric(0))
+
+results.frame <- rbind(results.frame, data.frame(TYPE=variable.type, AGE_RANGE=tag, CONDITIONS=extra.tag,
+                                                 AUC=round(mean(auc.vect), digits=2), 
+                                                 ACCURACY=round(mean(accuracy.vect), digits=2),
+                                                 SENSITIVITY=round(mean(sensitivity.vector), digits=2),
+                                                 SPECIFICITY=round(mean(specificity.vector), digits=2)))
+
+results.file <- paste(results.dir, variable.type, ".", extra.tag, ".", tag, ".results.txt", sep="")
+write.table(t(results.frame), file=results.file,
+            sep="\t", quote=F, col.names = F)
+
+
+## Plot Importance parameters
+levels(importance.frame$Parameter)
+
+## This reorders the legend
+importance.reorder <- reorder(importance.frame$Parameter, importance.frame$value, FUN=mean)
+levels.reordered <- rev(attributes(importance.reorder)$levels)
+
+importance.frame$Parameter <- factor(importance.frame$Parameter, levels = levels.reordered)
+
+## Calculate significance of differences and plot the importance
+matrix.frame <- read.table(input.file, sep="\t", header=T, na.strings = "NULL", 
+                           stringsAsFactors = F, colClasses = "numeric")
+
+matrix.good <- matrix.frame[matrix.frame$OUTPUT == 0,]
+matrix.bad <- matrix.frame[matrix.frame$OUTPUT == 1,]
+p.vector <- c()
+direction.vector <- c()
+median.direction.vector <- c()
+
+for(i in 1:length(levels.reordered)){
+  #colors <- c()
+  levels.reordered[i]
+  my.index <- match(levels.reordered[i], colnames(matrix.frame))
+  
+ if(length(unique(matrix.frame[,my.index]))>2){
+    ## Non-binary variable
+    good.vector <- matrix.good[,my.index]
+    bad.vector <- matrix.bad[,my.index]
+    
+    w <- wilcox.test(good.vector, bad.vector)
+    p.value <- w$p.value
+    my.ratio <- mean(good.vector)/mean(bad.vector)
+    my.median <- median(good.vector)/median(bad.vector)
+    p.vector <- c(p.vector, p.value)
+    direction.vector <- c(direction.vector, my.ratio)
+    median.direction.vector <- c(median.direction.vector, my.median)
+  }
+  else{
+    ## Binary variable
+    good.drug <- sum(matrix.good[,my.index])
+    good.nodrug <- length(matrix.good[,my.index]) - good.drug
+    
+    bad.drug <- sum(matrix.bad[,my.index])
+    bad.nodrug <- length(matrix.bad[,my.index]) - bad.drug
+    
+    contingency.matrix <- matrix(c(good.drug, bad.drug, good.nodrug, bad.nodrug), nrow = 2,
+                                 dimnames = list(c("GOOD", "BAD"), c("DRUG","NO_DRUG")))
+    f <- fisher.test(contingency.matrix)
+    p.value <- f$p.value
+    odds.ratio <- f$estimate
+    p.vector <- c(p.vector, p.value)
+    direction.vector <- c(direction.vector, odds.ratio)
+    median.direction.vector <- c(median.direction.vector, odds.ratio)
+  }
+}
+
+adjusted <- p.adjust(p.vector, method = "fdr")
+significance.frame <- data.frame(direction.vector, median.direction.vector, p.vector, adjusted)
+rownames(significance.frame) <- levels.reordered
+
+## Print significance results
+results.file <- paste(results.dir, variable.type, ".", extra.tag, ".", tag, ".variables.significance.txt", sep="")
+write.table(significance.frame, file=results.file,
+            sep="\t", quote=F, col.names = T)
+
+## And choose the colors
+colors.vector <- c()
+
+for(i in 1:length(levels.reordered)){
+  new.color <- "#999999"
+  if(significance.frame$direction.vector[i]>1){
+    new.color <- "#9999FF"
+    if(significance.frame$adjusted[i]<0.05){
+      new.color <- "#0000FF"
+    }
+  }
+  else if(significance.frame$direction.vector[i]<1){
+    new.color <- "#FF9999"
+    if(significance.frame$adjusted[i]<0.05){
+      new.color <- "#FF0000"
+    }
+  }
+  colors.vector <- c(colors.vector,new.color)
+}
+
+## And print the parameters
+p1<-ggplot(importance.frame, aes(x=reorder(Parameter, value, FUN=mean), y=value)) +
+  geom_boxplot(fill=rev(colors.vector), color=rev(colors.vector)) + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1),
+        panel.background = element_rect(fill="white", size=1, color="grey", linetype = "solid"),
+        panel.grid.major = element_line(size = 0.5, linetype = "solid", colour = "grey90")) + 
+  scale_y_continuous("Importance", limits=c(min(importance.frame$value), 
+                                            max(importance.frame$value))) + 
+  scale_x_discrete("Parameters")
+
+dat <- ggplot_build(p1)$data[[1]]
+p1 + geom_segment(data=dat, aes(x=xmin, xend=xmax, 
+                                y=middle, yend=middle), colour="grey90", size=0.8)
+
+
+
+p1.file <- paste(results.dir, "importance.", variable.type, ".", extra.tag, ".", tag, ".results.png", sep="")
+ggsave(p1.file, width=5, height = 4)
+dev.off()
+
+## Now with the median
+## And choose the colors
+median.colors.vector <- c()
+
+for(i in 1:length(levels.reordered)){
+  new.color <- "#999999"
+  if(significance.frame$adjusted[i]<0.05){
+    new.color <- "#666666"
+  }
+  
+  if(significance.frame$median.direction.vector[i]>1){
+    new.color <- "#9999FF"
+    if(significance.frame$adjusted[i]<0.05){
+      new.color <- "#0000FF"
+    }
+  }
+  else if(significance.frame$median.direction.vector[i]<1){
+    new.color <- "#FF9999"
+    if(significance.frame$adjusted[i]<0.05){
+      new.color <- "#FF0000"
+    }
+  }
+  median.colors.vector <- c(median.colors.vector, new.color)
+}
+
+p2<-ggplot(importance.frame, aes(x=reorder(Parameter, value, FUN=mean), y=value)) +
+  geom_boxplot(fill=rev(median.colors.vector), color=rev(median.colors.vector)) + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1),
+        panel.background = element_rect(fill="white", size=1, color="grey", linetype = "solid"),
+        panel.grid.major = element_line(size = 0.5, linetype = "solid", colour = "grey90")) + 
+  scale_y_continuous("Importance", limits=c(min(importance.frame$value), 
+                                            max(importance.frame$value))) + 
+  scale_x_discrete("Parameters")
+
+dat <- ggplot_build(p2)$data[[1]]
+p2 + geom_segment(data=dat, aes(x=xmin, xend=xmax, 
+                                y=middle, yend=middle), colour="grey90", size=0.8)
+
+p2.file <- paste(results.dir, "importance.", variable.type, ".", extra.tag, ".", tag, ".median.results.png", sep="")
+ggsave(p2.file, width=5, height = 4)
+dev.off()
